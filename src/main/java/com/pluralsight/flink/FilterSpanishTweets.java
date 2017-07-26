a5ecddc0ba2c60c2919d0400f260b70af10c1ac6
apardo <apardo@corunet.com>
2017-07-26 13:05:52 +0200

    Added config.properties file read for twitter values

--- src/main/java/com/pluralsight/flink/FilterSpanishTweets.java
+++ src/main/java/com/pluralsight/flink/FilterSpanishTweets.java
@@ -1,5 +1,9 @@
 package com.pluralsight.flink;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.apache.flink.api.common.functions.FilterFunction;
@@ -12,50 +16,68 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FilterSpanishTweets {
 
-    public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        
-        Properties props = new Properties();
-        props.setProperty(TwitterSource.CONSUMER_KEY, "yqNAHs4kvLAGx5JeOmt6A");
-        props.setProperty(TwitterSource.CONSUMER_SECRET, "c9zAhbP74VrLOV5MKxK8AfPL4oQfb9lieAiA8OyRQA");
-        props.setProperty(TwitterSource.TOKEN, "82975191-MldWNStfYekrHBVKcxGj3jIFRL94saJXkLGegJ9JT");
-        props.setProperty(TwitterSource.TOKEN_SECRET, "6JDYHwm4Fwuyb0zcaNlAoq4QDrjQkfqtgHdl3LNq9MIXe");
-        
-        env.addSource(new TwitterSource(props))
-        	.map(new MapToTweet())
-        	.filter(new FilterFunction<Tweet>() {
-        		@Override
-        		public boolean filter(Tweet tweet) throws Exception {
-        			return tweet.getLanguage().equals("es");
-        		}
-        	})
-        	.print();
-        
-        env.execute();
-    }
-    
-    private static class MapToTweet implements MapFunction<String, Tweet> {
+	public static void main(String[] args) throws Exception {
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-    	static private final ObjectMapper mapper = new ObjectMapper();
-    	
+		Properties props = new Properties();
+		InputStream input = null;
+
+		try {
+			String propFileName = "config.properties";
+			InputStream in = FilterSpanishTweets.class.getClassLoader().getResourceAsStream(propFileName);
+
+			if (in != null) {
+				props.load(in);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+			props.load(in);
+		} catch (IOException io) {
+			io.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+		env.addSource(new TwitterSource(props)).map(new MapToTweet()).filter(new FilterFunction<Tweet>() {
+			@Override
+			public boolean filter(Tweet tweet) throws Exception {
+				return tweet.getLanguage().equals("es");
+			}
+		}).print();
+
+		env.execute();
+	}
+
+	private static class MapToTweet implements MapFunction<String, Tweet> {
+
+		static private final ObjectMapper mapper = new ObjectMapper();
+
 		@Override
 		public Tweet map(String tweetJsonStr) throws Exception {
 			JsonNode tweetJson = mapper.readTree(tweetJsonStr);
 			JsonNode textNode = tweetJson.get("text");
 			JsonNode langNode = tweetJson.get("lang");
-			
+
 			String text = textNode == null ? "" : textNode.textValue();
 			String lang = langNode == null ? "" : langNode.textValue();
-			
+
 			return new Tweet(lang, text);
 		}
-    	
-    }
+
+	}
 }
 
 class Tweet {
-    private String language;
-    private String text;
+	private String language;
+	private String text;
+
 	/**
 	 * @param language
 	 * @param text
@@ -65,37 +87,43 @@ class Tweet {
 		this.language = language;
 		this.text = text;
 	}
+
 	/**
 	 * @return the language
 	 */
 	public String getLanguage() {
 		return language;
 	}
+
 	/**
 	 * @param language the language to set
 	 */
 	public void setLanguage(String language) {
 		this.language = language;
 	}
+
 	/**
 	 * @return the text
 	 */
 	public String getText() {
 		return text;
 	}
+
 	/**
 	 * @param text the text to set
 	 */
 	public void setText(String text) {
 		this.text = text;
 	}
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
 		return "Tweet [language=" + language + ", text=" + text + "]";
 	}
-    
-    
+
 }
